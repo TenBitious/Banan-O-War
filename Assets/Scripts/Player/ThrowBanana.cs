fa5508b415639c870ef0610a57897f1232376c36
Sly Emenike <sly.emenike@hva.nl>
2016-03-17 00:58:46 +0100

    Banana that throws

---
+++ Assets/Scripts/Player/ThrowBanana.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowBanana : MonoBehaviour {
+
+    public float throwSpeed;
+
+    public KeyCode keyThrow;
+
+    public ThrowingBanana prefabThrowingBanana;
+
+	// Use this for initialization
+	void Start () 
+    {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+	    //If button pressed, spawn banana
+        if (Input.GetKeyDown(keyThrow))
+        {
+            ThrowingBanana banana;
+            banana = Instantiate(prefabThrowingBanana, transform.position, transform.rotation) as ThrowingBanana;
+            banana.GetComponent<Rigidbody>().velocity = banana.transform.forward * throwSpeed;
+            //Intiatataete throwingBanana prefab
+        }
+	}
+}
---
+++ Assets/Scripts/Weapons/ThrowingBanana.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowingBanana : MonoBehaviour {
+
+    private Rigidbody myRigidbody;
+
+    public float rotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+        myRigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        //Rotate
+        transform.Rotate(new Vector3(rotationSpeed, 0, 0));
+    }
+
+    public void SetSpeed(float cSpeed)
+    {
+        Debug.Log("Banana speed:"+cSpeed);
+        myRigidbody.velocity =  new Vector3(0,0,cSpeed);
+    }
+}
