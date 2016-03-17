384695f4246bcda711830895d4a7ee3caf89a3fe
Rick Gijsen <rick.gijsen@hva.nl>
2016-03-17 17:22:27 +0100

    Tree growing

---
+++ Assets/Scripts/PlantedSeed.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PlantedSeed : MonoBehaviour {
+
+    public float evolutionTimeStart;
+    private float evolutionTime;
+    private bool canGrow;
+    private int evolutionState;
+	// Use this for initialization
+	void Start () {
+        evolutionTime = evolutionTimeStart;
+        canGrow = true;
+        evolutionState = 0;
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        if (canGrow)
+        {
+            Growing();
+        }
+    }
+
+    void Growing()
+    {      
+        evolutionTime -= Time.deltaTime;
+        Debug.Log(evolutionTime);
+        if (evolutionTime <= 0)
+        {
+            this.transform.GetChild(evolutionState).gameObject.SetActive(false);
+            this.transform.GetChild(evolutionState+1).gameObject.SetActive(true);
+            if (evolutionState < 3)
+            {
+                evolutionState += 1;
+                evolutionTime = evolutionTimeStart;
+            }
+        }
+
+    }
+
+    void NeedWater()
+    {
+    }
+}
---
+++ Assets/Scripts/Player/PlantSeed.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlantSeed : MonoBehaviour {
+
+    public KeyCode plantSeedKey;
+    public PlantedSeed prefabPlantedSeed;
+    public GameObject ground;
+
+    private bool canPlant;
+	// Use this for initialization
+	void Start () {
+        canPlant = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(plantSeedKey)&&canPlant)
+        {
+            PlantedSeed seed;
+            seed = Instantiate(prefabPlantedSeed, new Vector3(transform.position.x,ground.transform.position.y,transform.position.z), transform.rotation) as PlantedSeed;
+        }
+        else if (Input.GetKeyDown(plantSeedKey) && !canPlant)
+        {
+            Debug.Log("You can not plant");
+        }
+	}
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "Seed")
+        {
+            canPlant = false;            
+        }
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Seed")
+        {
+            canPlant = true;
+        }
+    }
+}
